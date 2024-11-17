d96ea91103346d31dc027ee7ee7b4e6c001bcae7
Mason <choimusong0529@gmail.com>
2024-11-17 15:33:03 +0900

    Initial Commit

---
+++ running-check/running_checkApp.swift
@@ -0,0 +1,17 @@
+//
+//  running_checkApp.swift
+//  running-check
+//
+//  Created by mason on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct running_checkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ running-checkTests/running_checkTests.swift
@@ -0,0 +1,16 @@
+//
+//  running_checkTests.swift
+//  running-checkTests
+//
+//  Created by mason on 11/17/24.
+//
+
+import Testing
+
+struct running_checkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
